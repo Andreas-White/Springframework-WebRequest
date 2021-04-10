b6f4820e41260f2750f46c9ac24358a9af13da0c
Andreas Manos <antmanos42@gmail.com>
2021-04-10 03:27:21 +0200

    adding POST request

--- src/main/java/com/example/controller/StudentController.java
+++ src/main/java/com/example/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.response.StudentResponse;
@@ -14,10 +15,16 @@ public class StudentController {
 	@Value("${app.name:Default Demo App}")
 	private String appName;
 	
-	@GetMapping("/get")
-	//@RequestMapping(value = "/get", method = RequestMethod.GET)
+	//@GetMapping("/get")
+	@RequestMapping(value = "/get", method = RequestMethod.GET)
 	public StudentResponse getStudent () {
 		StudentResponse studentResponse = new StudentResponse(1, "John", "Smith");
+		return studentResponse;
+	}
+
+	@RequestMapping(value = "/post", method = RequestMethod.POST)
+	public StudentResponse postStudent () {
+		StudentResponse studentResponse = new StudentResponse(1, "Andreas", "Smith");
 		return studentResponse;
 	}
 }
